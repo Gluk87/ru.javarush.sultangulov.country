ebb260d3873c4c3ed71aa4c85a9222feab738bfa
Azat S <diamond-city@yandex.ru>
2023-02-02 22:31:18 +0300

    Initial commit

---
+++ src/test/java/ru/javarush/country/CountryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javarush.country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
